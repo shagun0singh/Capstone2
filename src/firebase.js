27680f78f4fbb15384d9871f1dbe777be99a7d57
shagun0singh <shagun0singh@gmail.com>
2025-07-17 18:16:35 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyBCVjlw7FChn_5WF4w5rJ1sSn9NdbHULZY",
+  authDomain: "hydration-tracker-4345.firebaseapp.com",
+  projectId: "hydration-tracker-4345",
+  storageBucket: "hydration-tracker-4345.appspot.com",
+  messagingSenderId: "467872649434",
+  appId: "1:467872649434:web:bd6834f1b09cdfc85155e3",
+  measurementId: "G-EQYPTKBJ1E"
 };
 
 const app = initializeApp(firebaseConfig);
